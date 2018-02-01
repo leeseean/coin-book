3a19794f9386e362d6d2c2a1a1a9e425c73fbe81
leeseean chiu <leeseean@qq.com>
2018-02-02 01:05:23 +0800

    11

---
+++ src/assets/json/tableData.js
@@ -0,0 +1,61 @@
+export default [{
+  "date": "2017/7-2017/12/9",
+  "exchange": "biter&okcoin",
+  "money": "45000"
+},{
+  "date": "2017/12/9",
+  "exchange": "OKEX",
+  "money": "6587.49"
+}, {
+  "date": "2017/12/11",
+  "exchange": "ZB",
+  "money": "50000"
+}, {
+  "date": "2017/12/12",
+  "exchange": "GATE",
+  "money": "10050"
+}, {
+  "date": "2017/12/12",
+  "exchange": "OKEX",
+  "money": "-6791.26"
+}, {
+  "date": "2017/12/16",
+  "exchange": "GATE",
+  "money": "10050"
+}, {
+  "date": "2017/12/22",
+  "exchange": "ZB",
+  "money": "11000.00 "
+}, {
+  "date": "2017/12/24",
+  "exchange": "ZB",
+  "money": "100000.00 "
+}, {
+  "date": "2017/12/25",
+  "exchange": "ZB",
+  "money": "50000.00 "
+}, {
+  "date": "2017/12/26",
+  "exchange": "ZB",
+  "money": "28000"
+}, {
+  "date": "2018/1/2",
+  "exchange": "ZB",
+  "money": "33000"
+}, {
+  "date": "2018/1/10",
+  "exchange": "GATE",
+  "money": "20000"
+}, {
+  "date": "2018/1/16",
+  "exchange": "ZB",
+  "money": "6000"
+}, {
+  "date": "2018/1/25",
+  "exchange": "ZB",
+  "money": "-29700"
+}, {
+  "date": "2018/1/26",
+  "exchange": "GATE",
+  "money": "10000"
+}]
---
+++ src/components/HomePage.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-table :data="tableData" border stripe show-summary style="width: 600px;background-color: #252525;"  height="750">
+    <el-table-column prop="date" label="日期" width="200px">
+    </el-table-column>
+    <el-table-column prop="exchange" label="交易所" width="200px">
+    </el-table-column>
+    <el-table-column prop="money" label="收支金额(￥)" width="200px">
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import {
+    Table,
+    TableColumn
+  } from 'element-ui'
+  import tableData from '@/assets/json/tableData.js'
+  Vue.use(Table)
+  Vue.use(TableColumn)
+  export default {
+    name: 'HomePage',
+    data() {
+      return {
+        tableData
+      }
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+</style>
