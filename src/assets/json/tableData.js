87680792852a173646e96daa759754349a3e2449
leeseean chiu <leeseean@qq.com>
2018-06-17 16:10:38 +0800

    otcbtc

--- src/assets/json/tableData.js
+++ src/assets/json/tableData.js
@@ -1,85 +1,90 @@
+//-负号表示充值
 export default [{
   "date": "2017/7-2017/12/9",
   "exchange": "biter&okcoin",
-  "money": "45000"
+  "money": "-45000"
 },{
   "date": "2017/12/9",
   "exchange": "OKEX",
-  "money": "6587.49"
+  "money": "-6587.49"
 }, {
   "date": "2017/12/11",
   "exchange": "ZB",
-  "money": "50000"
+  "money": "-50000"
 }, {
   "date": "2017/12/12",
   "exchange": "GATE",
-  "money": "10050"
+  "money": "-10050"
 }, {
   "date": "2017/12/12",
   "exchange": "OKEX",
-  "money": "-6791.26"
+  "money": "6791.26"
 }, {
   "date": "2017/12/16",
   "exchange": "GATE",
-  "money": "10050"
+  "money": "-10050"
 }, {
   "date": "2017/12/22",
   "exchange": "ZB",
-  "money": "11000.00 "
+  "money": "-11000.00 "
 }, {
   "date": "2017/12/24",
   "exchange": "ZB",
-  "money": "100000.00 "
+  "money": "-100000.00 "
 }, {
   "date": "2017/12/25",
   "exchange": "ZB",
-  "money": "50000.00 "
+  "money": "-50000.00 "
 }, {
   "date": "2017/12/26",
   "exchange": "ZB",
-  "money": "28000"
+  "money": "-28000"
 }, {
   "date": "2018/1/2",
   "exchange": "ZB",
-  "money": "33000"
+  "money": "-33000"
 }, {
   "date": "2018/1/10",
   "exchange": "GATE",
-  "money": "20000"
+  "money": "-20000"
 }, {
   "date": "2018/1/16",
   "exchange": "ZB",
-  "money": "6000"
+  "money": "-6000"
 }, {
   "date": "2018/1/25",
   "exchange": "ZB",
-  "money": "-29700"
+  "money": "29700"
 }, {
   "date": "2018/1/26",
   "exchange": "GATE",
-  "money": "10000"
+  "money": "-10000"
 }, {
   "date": "2018/2/1",
   "exchange": "ZB",
-  "money": "-396"
+  "money": "396"
 }, {
   "date": "2018/2/16",
   "exchange": "ZB",
-  "money": "-495"
+  "money": "495"
 }, {
   "date": "2018/2/24",
   "exchange": "OKEX",
-  "money": "-1083.57"
+  "money": "1083.57"
 }, {
   "date": "2018/2/25",
   "exchange": "ZB",
-  "money": "-4554"
+  "money": "4554"
 }, {
   "date": "2018/2/26",
   "exchange": "ZB",
-  "money": "-15245.01"
+  "money": "15245.01"
 }, {
   "date": "2018/2/25",
   "exchange": "GATE",
-  "money": "-12924.91"
+  "money": "12924.91"
+}, {
+  "date": "2018/6/17",
+  "exchange": "OTCBTC",
+  "money": "-3338.38"
 }]
